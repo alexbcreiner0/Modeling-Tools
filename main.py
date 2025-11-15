4448320341e150e2244e0dfc56d2669c601038cc
Alex Creiner <alexbcreiner@gmail.com>
2025-11-15 02:11:00 -0500

    2025-11-15

--- main.py
+++ main.py
@@ -61,5 +61,8 @@ if __name__ == "__main__":
     app.exec()
 
     with open("dimensions.txt", "w") as f:
-        print(window.graph_panel.xlim, file= f)
-        print(window.graph_panel.ylim, file= f)
+        xlim, ylim = window.graph_panel.xlim, window.graph_panel.ylim
+        xlim_float = (float(xlim[0]), float(xlim[1]))
+        ylim_float = (float(ylim[0]), float(ylim[1]))
+        print(xlim_float, file= f)
+        print(ylim_float, file= f)
