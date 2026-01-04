9c51e5888e4283b89daefb9970580aa112ecb34d
Alex Creiner <142434892+alexbcreiner0@users.noreply.github.com>
2026-01-04 00:36:56 -0500

    Update loader.py

--- tools/loader.py
+++ tools/loader.py
@@ -9,11 +9,11 @@ from paths import rpath
 
 def load_presets(path):
     try:
-        with open(rpath(f"models/{path}/data/params.yml"), 'r') as f:
+        with open(rpath("models",path,"data","params.yml"), 'r') as f:
             doc = yaml.safe_load(f)
         return doc["presets"]
     except (FileNotFoundError, KeyError, TypeError):
-        with open(rpath(f'models/{path}/data/extra_data.yml'), 'r') as f:
+        with open(rpath("models",path,"data","extra_data.yml"), 'r') as f:
             default_presets = yaml.safe_load(f)
         _dump_to_yaml(default_presets, path)
         return default_presets
@@ -37,7 +37,7 @@ def _dump_to_yaml(presets, path):
         width=88
     )
 
-    with open(f"models/{path}/data/params.yml", "w") as f:
+    with open(rpath("models",path,"data","params.yml"), "w") as f:
         f.write(text)
 
 class NoAliasDumper(yaml.SafeDumper):
